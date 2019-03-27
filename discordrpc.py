e871ee291c36799895e87597f4e48907ffa2ebb8
耀風 <adwin82627459@gmail.com>
2019-03-27 11:31:10 +0800

    更新到V0.1&加入設定檔支援

--- discordrpc.py
+++ discordrpc.py
@@ -1,4 +1,14 @@
 import pypresence
+import toml
+import os
+configfile = os.path.join('.','config.toml')
+
+try:
+	config = toml.load(configfile)
+	print('成功載入' + configfile)
+except:
+	print(f"無法載入{configfile}")
+	input()
 
 def menu():
 	print('如要退出請輸入"exit"或按右上角X')
@@ -6,26 +16,18 @@ def menu():
 	if a == 'exit':
 		exit()
 	else:
-		test()
+		menu()
 
-rpc = pypresence.Presence(ClientID)
-rpc.connect()
-rpc.update(state="",
-details="",
-large_image="",
-large_text="",
-small_image="",
-small_text="")
+def rpc():
+	rpc = pypresence.Presence(config['rpcid']['id'])
+	rpc.connect()
+	rpc.update(state=config['contect']['state'],
+	details=config['contect']['details'],
+	large_image=config['contect']['large_image'],
+	large_text=config['contect']['large_text'],
+	small_image=config['contect']['small_image'],
+	small_text=config['contect']['small_text'])
 
+rpc()
 print('RPC已啟動請查看Discord遊戲狀態')
 menu()
-
-'''
-rpc.update(state=format_state(data),
-	details=format_details(data),
-	start=format_start(data),
-	large_image=format_large_image(data),
-	large_text=format_large_text(data),
-	small_image=format_small_image(data),
-	small_text=format_small_text(data),
-	party_size=format_party_size(data))'''
